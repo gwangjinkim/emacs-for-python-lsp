5da74b525ecb3e8532a2c907fba6510af0a302ab
gwangjinkim <gwang.jin.kim.phd@gmail.com>
2024-07-21 22:51:33 +0200

    better theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,7 +37,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(elpy try use-package)))
+ '(package-selected-packages '(dap-mode elpy try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
